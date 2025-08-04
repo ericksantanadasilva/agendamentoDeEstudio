842443f2d87e00aa217deeab41f9fe700cac4b71
Erick Sant'Ana da Silva <ericksans199@outlook.com>
2025-08-03 23:57:24 -0300

    integração do supabase para carregar os eventos

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -3,13 +3,63 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import '@fullcalendar/core/locales/pt-br';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../styles/fullcalendar-overrides.css';
+import { supabase } from '../lib/supabase';
 
 export default function Calendar() {
+  const [events, setEvents] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
   useEffect(() => {
-    document.body.classList.add('dark'); // Ativa o modo escuro do Tailwind
+    const fetchEvents = async () => {
+      const { data, error } = await supabase.from('agendamentos').select('*');
+
+      if (error) {
+        console.error('Erro ao carregar agendamentos: ', error);
+        return;
+      }
+
+      const formatted = data.map((evento) => {
+        const startDateTime = `${evento.date}T${evento.start}`;
+        const endDateTime = `${evento.date}T${evento.end}`;
+
+        return {
+          id: evento.id,
+          title: `${evento.professor} - ${evento.materia}`,
+          start: startDateTime,
+          end: endDateTime,
+          extendedProps: {
+            studio: evento.studio,
+            tecnico: evento.tecnico,
+            gravacao: evento.gravacao,
+            tipo: evento.tipo,
+            user_email: evento.user_email,
+            user_id: evento.user_id,
+          },
+        };
+      });
+      setEvents(formatted);
+      console.log('dados recebidos do supabase: ', data);
+      console.log('Eventos formatados: ', formatted);
+    };
+    fetchEvents();
   }, []);
+
+  const handleDateClick = (info) => {
+    setSelectedDate(info.dateStr);
+    setSelectedEvent(null);
+    // abrir modal de criação aqui
+    console.log('abrir modal de criação para ', info.dateStr);
+  };
+
+  const handleEventClick = (info) => {
+    setSelectedEvent(info.event.extendedProps);
+    setSelectedDate(null);
+    //abrir modal de ediçao aqui
+    console.log('abrir modal de ediçao para ', info.event.extendedProps);
+  };
 
   return (
     <div className='px-4 sm:px-6 lg:px-8 max-w-screen overflow-x-hidden'>
@@ -41,6 +91,9 @@ export default function Calendar() {
           'bg-blue-600 text-white text-sm px-2 py-1 rounded shadow'
         }
         dayHeaderClassNames={() => 'text-zinc-100 bg-zinc-800 py-2'}
+        events={events}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
       />
     </div>
   );
